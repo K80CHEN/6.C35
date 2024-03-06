142dfa327fd6de93497c3f0737c3cde6d1741542
K80Kat <mzc219@nyu.edu>
2024-03-06 09:41:07 -0500

    lab4 in progress

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+
+<!-- add title using <svelte:head> -->
+    <svelte:head>
+        <title>Homepage</title>
+    </svelte:head>
+
+<h1>Katie-test: Personal site and portfolio</h1>
+<p>Hi, I'm Katie. I'm a web developer and designer. I'm currently working on my personal site and portfolio. Check back soon for updates!</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea quia aspernatur mollitia pariatur cupiditate inventore et? Voluptatum, iste praesentium corrupti culpa itaque, repudiandae at, porro veniam doloremque dolor dolorum perferendis.</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus libero commodi vel dolore consectetur molestiae eligendi, delectus veritatis blanditiis facilis vero nesciunt. Excepturi distinctio impedit odio hic consequatur in ipsum.</p>
+<img src="./images/buddy.JPG" alt="Buddy, a white samoyed dog, who was my dog and also was my family." width="100px">
+<p>please enjoy an actual photo of my dog</p>
+
---
+++ src/routes/project/+page.svelte
@@ -0,0 +1,68 @@
+
+<svelte:head>
+    <title>Project</title>
+</svelte:head>
+
+
+    <div class="projects">
+        <article>
+            <h2>Lorem ipsum dolor sit.</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Sapiente placeat aliquam animi!</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Voluptatem earum quos nisi.</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Saepe facere reprehenderit laboriosam!</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Est necessitatibus commodi dignissimos.</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Veritatis harum laboriosam nostrum?</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Porro enim nesciunt quaerat.</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Labore eius fugiat iste!</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Distinctio laborum sed assumenda?</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Odio laudantium natus vitae.</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Sapiente enim magnam repellendus!</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+        <article>
+            <h2>Rem, hic. Possimus, debitis!</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p></p>
+        </article>
+    </div>
